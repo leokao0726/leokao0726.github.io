3ccc0e26f67737553bca944ed6bd8f9d075cacb5
Leo Kao <leokao0726@gmai.com>
2017-11-22 02:30:46 +0800

    add social link and let down icon smooth to about place

--- js/main.js
+++ js/main.js
@@ -9,4 +9,15 @@ $(document).ready(function(){
 		$('#menu-trigger').click();
 	});
 
+	$('#goto-second-page').click(function(e){
+		$("html, body").animate({ scrollTop: $('#about').offset().top }, 1000);	
+	});
+
+	$('.header img').mouseenter(function(){
+		$('.about-link').slideDown();
+
+	});
+	$('.about-link li').mouseout(function(){
+		$('.about-link').slideUp();
+	});
 });
